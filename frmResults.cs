f037cae6e54b07d4e089fe7069a215b23255c810
TiagoPillon <tiagoplsilva@gmail.com>
2016-11-21 20:24:54 -0200

    Update frmResults.cs

--- frmResults.cs
+++ frmResults.cs
@@ -23,25 +23,4 @@ namespace Simplex
             panContent.Controls.Add(controle);            
             
         }
-
-        private void toolStripButton1_Click(object sender, EventArgs e)
-        {
-            if (printDialog1.ShowDialog(this) == DialogResult.OK)
-            {
-                printDocument1.DocumentName = "Simplex " + DateTime.Now.ToString("HH:mm:ss");
-                printDocument1.PrinterSettings = printDialog1.PrinterSettings;
-                printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(printDocument1_PrintPage);
-                printDocument1.Print();
-            }
-            
-        }
-
-        void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
-        {
-            Bitmap b = new Bitmap(panContent.Width, panContent.Height);
-            panContent.DrawToBitmap(b, new Rectangle(0,0,panContent.Width, panContent.Height));
-            e.Graphics.DrawImage(b, new Point(0, 0));
-            
-        }
-    }
 }
